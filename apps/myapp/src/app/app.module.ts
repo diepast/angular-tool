4453f0acf836b85b55cca32ed7af1d313ccf2d31
diego <dmpastuch@gmail.com>
2021-06-05 10:28:23 -0300

    first commit

--- apps/myapp/src/app/app.module.ts
+++ apps/myapp/src/app/app.module.ts
@@ -3,10 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BoardComponentComponent } from './board-component/board-component.component';
+import { BoardComponent } from './board/board.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    BoardComponentComponent,
+    BoardComponent
   ],
   imports: [
     BrowserModule,
---
+++ apps/myapp/src/app/board-component/board-component.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-board-component',
+  templateUrl: './board-component.component.html',
+  styleUrls: ['./board-component.component.css']
+})
+export class BoardComponentComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
